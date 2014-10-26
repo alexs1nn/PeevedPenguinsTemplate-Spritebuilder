c3afceeab9d46076c9bb442c4e5c64521202b904
Alexander Savelyev <alexanders@ivendy.com>
2014-10-26 09:30:44 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Alexander Savelyev on 10/26/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by Alexander Savelyev on 10/26/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
